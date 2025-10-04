d26103d95d08eaa3bc9e84d6f7904f522ba98b43
obymael <82178035+obymael@users.noreply.github.com>
2025-10-04 17:01:49 +0200

    #5 Import pages and update paths

--- web-client/src/routers/index.js
+++ web-client/src/routers/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+import AboutPage from '../views/AboutPage.vue'
 import MonitorPage from '../views/MonitorPage.vue'
+import ContactPage from '../views/ContactPage.vue'
 import FAQ from '../views/FaqPage.vue'
 
 const routes = [
+  { path: '/', name: 'Home', component: HomePage },
+  { path: '/', name: 'About', component: AboutPage },
   { path: '/', name: 'Monitor', component: MonitorPage },
+  { path: '/', name: 'Contact', component: ContactPage },
   { path: '/faq', name: 'FAQ', component: FAQ }
 ]
 
